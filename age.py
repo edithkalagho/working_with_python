cad2f804170caaad5d7e6c7e720e3726bb3c6eaa
edithkalagho <edithkalagho@gmail.com>
2024-05-28 14:25:40 +0200

    updated

---
+++ age.py
@@ -0,0 +1,5 @@
+
+dob = int(input("which year were you born? "))
+
+age = 2024 - dob 
+print(age)
---
+++ area.py
@@ -0,0 +1,8 @@
+"""
+calculate area of a triangle
+"""
+base = 27
+height = 12
+
+area = 1/2 * base * height
+print("the area is ", area)
